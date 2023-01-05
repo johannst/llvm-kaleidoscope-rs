617f0d65b20b7b405b1acecea6f99ccd0a6d73f3
johannst <johannst@users.noreply.github.com>
2023-01-05 20:25:01 +0000

    deploy: 802cca1963bc27f8ea4e4923029909e45464d3df

--- implementors/core/convert/trait.TryFrom.js
+++ implementors/core/convert/trait.TryFrom.js
@@ -1,3 +1,3 @@
-(function() {var implementors = {};
-implementors["llvm_kaleidoscope_rs"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.63.0/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;&amp;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.63.0/std/primitive.str.html\">str</a>&gt; for <a class=\"struct\" href=\"llvm_kaleidoscope_rs/struct.SmallCStr.html\" title=\"struct llvm_kaleidoscope_rs::SmallCStr\">SmallCStr</a>","synthetic":false,"types":["llvm_kaleidoscope_rs::SmallCStr"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {var implementors = {
+"llvm_kaleidoscope_rs":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.65.0/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;&amp;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.65.0/std/primitive.str.html\">str</a>&gt; for <a class=\"struct\" href=\"llvm_kaleidoscope_rs/struct.SmallCStr.html\" title=\"struct llvm_kaleidoscope_rs::SmallCStr\">SmallCStr</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- source-files.js
+++ source-files.js
@@ -1,3 +1,4 @@
-var sourcesIndex = {};
-sourcesIndex["llvm_kaleidoscope_rs"] = {"name":"","dirs":[{"name":"llvm","files":["basic_block.rs","builder.rs","lljit.rs","mod.rs","module.rs","pass_manager.rs","type_.rs","value.rs"]}],"files":["codegen.rs","lexer.rs","lib.rs","parser.rs"]};
+var sourcesIndex = JSON.parse('{\
+"llvm_kaleidoscope_rs":["",[["llvm",[],["basic_block.rs","builder.rs","lljit.rs","mod.rs","module.rs","pass_manager.rs","type_.rs","value.rs"]]],["codegen.rs","lexer.rs","lib.rs","parser.rs"]]\
+}');
 createSourceSidebar();
